f0c6c43dd222d53b6f0211a93512df1f41c199eb
Pravin Raj Rajendran <pravinraj.rajendran@syncfusion.com>
2021-11-30 17:13:32 +0530

    Release azure package changes for v3.3.23

---
+++ reporting/web/wwwroot/cdn/scripts/localization/datatileconfiguration/ej.localetexts.de-DE.js
@@ -0,0 +1,15 @@
+ej.DataTileConfiguration.Locale['de-DE'] = {
+    searchBoxPlaceholder: 'Such-Connector',
+    category: {
+        databases: 'Datenbanken',
+        files: 'Dateien',
+        services: 'Dienstleistungen'
+    },
+    enableAllCheckbox: 'Alles aktivieren',
+    selectedText: ' von {count} ausgew�hlt',
+    noDataText: 'Keine Datenquellen gefunden',
+    alertHeaderText: 'BerichtsDesigner',
+    saveAlertContent: 'Fehler beim Speichern der Verbindungslistendetails.',
+    chooseAlertContent: 'Bitte w�hlen Sie mindestens eine Verbindung aus.',
+    failedAlertContent: 'Verbindungsinformationen konnten nicht aktualisiert werden.'
+};
