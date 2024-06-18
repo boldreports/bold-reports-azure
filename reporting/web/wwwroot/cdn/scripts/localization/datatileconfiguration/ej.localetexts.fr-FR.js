466d8ce9a1a1b12f81a8d94f6facdce103fe763b
padmanaban <padmanaba.n@syncfusion.com>
2024-06-18 17:15:30 +0530

    Adding v6.1.45 files

---
+++ reporting/web/wwwroot/cdn/scripts/localization/datatileconfiguration/ej.localetexts.fr-FR.js
@@ -0,0 +1,15 @@
+ej.DataTileConfiguration.Locale['fr-FR'] = {
+    searchBoxPlaceholder: 'Connecteur de recherche',
+    category: {
+        databases: 'Bases de données',
+        files: 'Des dossiers',
+        services: 'Prestations de service'
+    },
+    enableAllCheckbox: 'Tout activer',
+    selectedText: ' de {count} choisi',
+    noDataText: 'Aucune source de données trouvée',
+    alertHeaderText: 'Concepteur de rapports',
+    saveAlertContent: 'Échec de l\'enregistrement des détails de la liste de connexions.',
+    chooseAlertContent: 'Veuillez sélectionner au moins une connexion.',
+    failedAlertContent: 'Échec de la mise à jour des informations de connexion.'
+};
