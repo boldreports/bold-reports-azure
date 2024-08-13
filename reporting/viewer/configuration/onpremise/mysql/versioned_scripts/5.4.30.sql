54658cf1917d8867a5fad83f902becbebb1c8169
padmanaban <padmanaba.n@syncfusion.com>
2024-08-13 11:51:44 +0530

    Azure package changes for bold reports 6.2.32 release

--- reporting/viewer/configuration/onpremise/mysql/versioned_scripts/5.4.30.sql
+++
@@ -1,4 +0,0 @@
-INSERT into {database_name}.BOLDRS_SettingsType (Name,IsActive) VALUES ('CORS Settings',1)
-;
-INSERT into {database_name}.BOLDRS_SettingsType (Name,IsActive) VALUES ('Security',1)
-;
