3c94c3115d833b3c2d4ec67b34aae5ad1ef6c6fa
padmanaban <padmanaba.n@syncfusion.com>
2024-09-10 15:56:48 +0530

    Update the Azure Package v6.2.33

--- reporting/api/configuration/onpremise/postgresql/versioned_scripts/latest.sql
+++ reporting/api/configuration/onpremise/postgresql/versioned_scripts/latest.sql
@@ -1,3 +1,7 @@
+ALTER TABLE BOLDRS_ScheduleDetail ADD IsEmailAttachment smallint NOT NULL DEFAULT 1
+;
+ALTER TABLE BOLDRS_ScheduleDetail ADD IsInsertReportInMailBody smallint NOT NULL DEFAULT 0
+;
 ALTER TABLE BOLDRS_ScheduleDetail ADD COLUMN IsNoDataEnabled smallint NULL;
 ALTER TABLE BOLDRS_Item ADD ViewerFeatures varchar(4000) NULL
 ;
