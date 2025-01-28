7217d5ffb42567662bdfdf7cb04e97e5ebdc6220
root <root@syncfusion.nsxpqd4t5xmupbjh00jbvghc0c.bx.internal.cloudapp.net>
2025-01-28 11:09:30 +0000

    updating v6.3.25_E18422

---
+++ reporting/web/wwwroot/extensions/extension.config.js
@@ -0,0 +1,37 @@
+var dataExtensions = [];
+var cloudDataExtensions = [];
+var itemExtensions = [{
+        name: 'barcode',
+        className: 'EJBarcode',
+        imageClass: 'customitem-barcode',
+        displayName: '1D Barcode',
+        category: 'Barcodes',
+        toolTip: {
+            requirements: 'Add a report item to the designer area.',
+            description: 'Display the barcode lines as report item.',
+            title: 'Barcode'
+        }
+    }, {
+        name: 'matrixbarcode',
+        className: 'EJQRBarcode',
+        imageClass: 'customitem-qrbarcode',
+        displayName: '2D Barcode',
+        category: 'Barcodes',
+        toolTip: {
+            requirements: 'Add a report item to the designer area.',
+            description: 'Display the barcode lines as report item.',
+            title: '2D Barcode'
+        }
+    },
+    {
+        name: 'ESignature',
+        className: 'EJSignature',
+        imageClass: 'customitem-signature',
+        displayName: 'Electronic',
+        category: 'Signature',
+        toolTip: {
+            requirements: 'Add a report item to the designer area.',
+            description: 'This report item is used to add a graphic signature.',
+            title: 'Signature'
+        }
+    }];
