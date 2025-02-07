d22b6d76222641d41f3fd12885d510225c74de8c
root <root@Linux-Fresh-case-VM.m3xv35ptgoyujlnxcwiqpsujfb.bx.internal.cloudapp.net>
2025-02-07 09:53:52 +0000

    committing final build changes

--- idp/ums/wwwroot/scripts/system/inputboxcustom.js
+++ idp/ums/wwwroot/scripts/system/inputboxcustom.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
     radioButtonInitialization("#file-storage", "File Storage", "IsBlobStorage", true, "0");
     radioButtonInitialization("#blob-storage", "Azure Blob Storage", "IsBlobStorage", false, "1");
     
-    radioButtonInitialization("#single-database", "Single Database for All Tenants ", "ConfigurationMode", true, "0");
-    radioButtonInitialization("#database-per-tenant", "Database Per Tenant", "ConfigurationMode", false, "1")
+    radioButtonInitialization("#single-database", "Shared Database", "ConfigurationMode", true, "0");
+    radioButtonInitialization("#database-per-tenant", "Separate Database", "ConfigurationMode", false, "1")
     
     radioButtonInitialization("#https", "Use HTTPS (Recommended)", "Connection", true, "https");
     radioButtonInitialization("#http", "Use HTTP", "Connection", false, "http");
