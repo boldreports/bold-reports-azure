ee347a7ace9b8d0dae22f815abb72d8031a65dee
Gunachandar8 <gunachandar.n@syncfusion.com>
2025-06-05 18:39:22 +0530

    Move the custom changes in latest version

--- reporting/api/configuration/onpremise/oracle/versioned_scripts/latest.sql
+++ reporting/api/configuration/onpremise/oracle/versioned_scripts/latest.sql
@@ -1,1 +1,8 @@
 ALTER TABLE BOLDRS_ItemLog MODIFY AdditionalLogInfo VARCHAR2(4000);
+
+ALTER TABLE BOLDRS_ScheduleDetail ADD IsEmailAttachment NUMBER(1) DEFAULT 1 NOT NULL;
+
+ALTER TABLE BOLDRS_ScheduleDetail ADD IsInsertReportInMailBody NUMBER(1) DEFAULT 0 NOT NULL;
+
+INSERT INTO BOLDRS_GroupPermission (PermissionAccessId,PermissionEntityId,ItemId,GroupId,IsActive) VALUES (16,'17',NULL,'1','1')
+;
